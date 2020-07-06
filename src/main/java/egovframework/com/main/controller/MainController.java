c751864db2e41c91e0881d0b795623472c7e4d5c
a2 <a2@192.168.0.3>
2020-07-06 09:41:16 +0900

    Vo, modelattribute 테스

--- src/main/java/egovframework/com/main/controller/MainController.java
+++ src/main/java/egovframework/com/main/controller/MainController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import egovframework.com.main.service.MainService;
 
 @Controller
-@RequestMapping(value = "/main")
+@RequestMapping(value = "/main/*")
 public class MainController {
 
 	@Resource(name = "mainService")
@@ -23,19 +23,26 @@ public class MainController {
 		System.out.println("main controller");
 		
 		try {
-//			for (int i = 0; i < 5; i++) {
-//				Map<String, String> dummy = new HashMap();
-//				
-//				dummy.put("name", "name " + i);
-//				dummy.put("position", "position " + i);
-//				dummy.put("office", "office " + i);
-//				dummy.put("age", "age " + i);
-//				dummy.put("startDate", "startDate " + i);
-//				dummy.put("salary", "salary " + i);
-//				
-//				list.add(dummy);
-//			}
-//			System.out.println(list);
+
+			
+			List<Map<String, String>> dummyList = mainService.selectDummyList();
+			System.out.println("dummyList >>> " + dummyList);
+			
+			model.addAttribute("resultList", dummyList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "main/main";
+	}
+	
+
+	@RequestMapping(value = "/main2.do")
+	public String home2(ModelMap model) throws Exception{
+		System.out.println("main controller2");
+		
+		try {
+
 			
 			List<Map<String, String>> dummyList = mainService.selectDummyList();
 			System.out.println("dummyList >>> " + dummyList);
---
+++ src/main/java/egovframework/com/sub/vo/SubVo.java
@@ -0,0 +1,15 @@
+package egovframework.com.sub.vo;
+
+public class SubVo {
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
