8159e711e6ff55951d0e7da4f99354863ac8612d
a2 <a2@192.168.1.131>
2020-07-14 11:17:00 +0900

    cmmninterceptor

--- src/main/java/egovframework/com/cmmn/interceptor/cmmnInterceptor.java
+++ src/main/java/egovframework/com/cmmn/interceptor/cmmnInterceptor.java
@@ -2,14 +2,19 @@ package egovframework.com.cmmn.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import egovframework.com.user.vo.UserVo;
+
 public class cmmnInterceptor extends HandlerInterceptorAdapter {
 
+	private static final String LOGIN = "login";
 	protected Log log = LogFactory.getLog(cmmnInterceptor.class);
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -30,12 +35,28 @@ public class cmmnInterceptor extends HandlerInterceptorAdapter {
 			ModelAndView modelAndView) throws Exception {
 		// TODO Auto-generated method stub
 		
-		if(log.isDebugEnabled()) {
-			log.debug(" ============= end ==============");
-			if (modelAndView != null) {
-				log.debug("Response viewName : " + modelAndView.getViewName());
+		HttpSession httpSession = request.getSession();
+		String requestURI = request.getRequestURI();
+		
+		try {
+			if(!requestURI.equals("/main/main.do")) {
+				if(httpSession.getAttribute(LOGIN) != null) {
+					if(log.isDebugEnabled()) {
+						log.debug(" ============= end ==============");
+						if (modelAndView != null) {
+							log.debug("Response viewName : " + modelAndView.getViewName());
+						}
+					}
+				}else {
+					log.debug(" =============권한없음==============");
+					response.sendRedirect("/main/main.do?chk=1");
+				}
 			}
+		}catch(Exception e) {
+		
 		}
+		
+	
 		
 		super.postHandle(request, response, handler, modelAndView);
 	}
